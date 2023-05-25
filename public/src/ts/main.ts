070c523e105f13c77a37a735beff76a979ae12f6
JoaoGSDC <joao.cruz@esx.com.br>
2023-05-25 00:37:42 -0300

    build folder adjustment

--- public/src/ts/main.ts
+++ public/src/ts/main.ts
@@ -3,6 +3,8 @@ import "../scss/main.scss"
 import Render from "./render"
 import Locales from "../utils/locales"
 
+console.log("teste")
+
 document.addEventListener("DOMContentLoaded", () => {
   localStorage.setItem("language", navigator.language)
   Locales.loadTranslations(navigator.language)
