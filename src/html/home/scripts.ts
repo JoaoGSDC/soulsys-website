848dba7e87eba5971e456458c422c711b6a606e1
JoaoGSDC <joao.cruz@esx.com.br>
2023-05-24 23:42:23 -0300

    build adjustments

--- src/html/home/scripts.ts
+++ src/html/home/scripts.ts
@@ -1,17 +1,5 @@
 import section from "../../ts/section"
 
 export default function Home() {
-  function handleListenerScrollEvent() {
-    window.addEventListener("scroll", () => {
-      let scrollPosition = window.scrollY
-      let image = document.getElementById("logocontainer") as HTMLElement
-      let imageHeight = image.offsetHeight
-      let windowHeight = window.innerHeight
-      let distanceFromTop = image.offsetTop - scrollPosition
-      let percentage = 1 - distanceFromTop / (windowHeight + imageHeight)
-      image.style.transform = "scale(" + percentage + ")"
-    })
-  }
-
   section.render("home")
 }
--- src/ts/watching-sections.ts
+++ src/ts/watching-sections.ts
@@ -5,15 +5,15 @@ export function watchSections(activeClassName: string, tagToAnimate: string) {
     console.log(animatedSections)
 
     const options = {
-      threshold: 0.5, // A animação será ativada quando a seção estiver 50% visível na tela
+      threshold: 0.5,
     }
 
-    const observer = new IntersectionObserver(function (entries, observer) {
+    const observer = new IntersectionObserver(function (entries) {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.remove(activeClassName) // Adiciona a classe "ativa" para iniciar a animação
+          entry.target.classList.remove(activeClassName)
         } else {
-          entry.target.classList.add(activeClassName) // Remove a classe "ativa" para interromper a animação
+          entry.target.classList.add(activeClassName)
         }
       })
     }, options)
