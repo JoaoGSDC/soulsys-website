e2c78ddffc484860353d1eb4395d6aadb8bc12e8
JoaoGSDC <joao.cruz@esx.com.br>
2023-05-25 22:39:12 -0300

    adjustments in smartphone view

--- public/src/components/footer/scripts.ts
+++ public/src/components/footer/scripts.ts
@@ -1,5 +1,35 @@
 import component from "../../ts/component"
 
 export default function Footer() {
+  function scrollToSection(e: any) {
+    const { id } = e.target
+
+    if (
+      ![
+        "footer__start-button",
+        "footer__solutions-button",
+        "footer__contact-button",
+      ].includes(id)
+    ) {
+      return
+    }
+
+    const sectionIds: any = {
+      "footer__start-button": "#home",
+      "footer__solutions-button": "#web-solution",
+      "footer__contact-button": "#contact",
+    }
+    const sectionId = sectionIds[id]
+    const section = document.querySelector(sectionId)
+
+    if (!section) {
+      return
+    }
+
+    section.scrollIntoView({ behavior: "smooth" })
+  }
+
+  window.addEventListener("click", scrollToSection)
+
   component.render("footer")
 }
